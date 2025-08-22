15edb40e9a6204e5573f2e32674ba21f7a0e39ca
DavCode1 <davidmnr571@gmail.com>
2025-08-22 10:02:24 +0200

    Ajout API url + ajout btn add contact

--- src/pages/Contacts.page.tsx
+++ src/pages/Contacts.page.tsx
@@ -1,9 +1,28 @@
 import { ContactsList } from '../features/contact/ContactsList';
 
 export const ContactsPage = () => {
+  const handleAddContact = () => {
+    window.location.href = '/contacts/add'; 
+  };
+
   return (
     <>
       <h1>My contacts list</h1>
+
+      <button 
+        onClick={handleAddContact} 
+        style={{
+          padding: '8px 16px',
+          marginBottom: '16px',
+          backgroundColor: '#1976d2',
+          color: 'white',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer'
+        }}
+      >
+        Add Contact
+      </button>
 
       <ContactsList />
     </>
